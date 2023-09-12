f9b9d9f46f3d4ca6de1811bc2e152b29356d1a36
Rinidh <rinidhdhokia@gmail.com>
2023-09-12 17:04:15 +0300

    Supply other fonts when kanit font unavailable. Add Swtch in About.tsx to disable demo mode

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,18 +1,30 @@
 import {
+  Box,
   Button,
+  Divider,
   Drawer,
   DrawerBody,
   DrawerContent,
   DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
+  Switch,
   Text,
+  useColorMode,
   useDisclosure,
 } from "@chakra-ui/react";
 import { BsQuestionOctagonFill } from "react-icons/bs";
+import styled from "styled-components";
+
+const StyledInlineHeading = styled.p`
+  display: inline-block;
+  font-size: 25px;
+`; //used this coz the <Text> comp of Chakra has unchangeable display: block style, yet inline-block is needed
 
 function About() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode } = useColorMode();
+
   return (
     <>
       <Button
@@ -20,14 +32,10 @@ function About() {
         children={<BsQuestionOctagonFill />}
         size={"lg"}
       />
-      <Drawer placement={"top"} onClose={onClose} isOpen={isOpen}>
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
         <DrawerOverlay />
         <DrawerContent>
-          <DrawerHeader
-            borderBottomWidth="5px"
-            fontSize={30}
-            textAlign={"center"}
-          >
+          <DrawerHeader fontSize={30} textAlign={"center"}>
             About this App
           </DrawerHeader>
 
@@ -38,9 +46,35 @@ function About() {
             </Text>
           </DrawerBody>
 
-          <DrawerFooter>{/* can place some button here */}</DrawerFooter>
+          <DrawerFooter>
+            <Box
+              bg={colorMode === "light" ? "#ffcdde" : "#710d0d"}
+              borderRadius={20}
+              padding={3}
+            >
+              <Text fontSize={20}>
+                You are currently running this app in demonstration mode. Any
+                search will only return this project's owner's github
+                information only
+              </Text>
+              <Divider colorScheme="red" marginBottom={10} />
+
+              <Box width={"190px"}>
+                <StyledInlineHeading>
+                  Activate Github Searches
+                </StyledInlineHeading>
+              </Box>
+              <Switch
+                colorScheme="red"
+                size="md"
+                display={"inline-block"}
+                float={"right"}
+                marginTop={-12} //negative values for positioning
+              />
+            </Box>
+          </DrawerFooter>
         </DrawerContent>
-      </Drawer>{" "}
+      </Drawer>
     </>
   );
 }
