314ea37e86ec8337b7abf3571feac329fb8a2d3a
Rinidh <rinidhdhokia@gmail.com>
2023-09-22 22:45:25 +0300

    Error messages working, and display the collected data

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -26,6 +26,8 @@ function Form() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   //creating the schema and type. And using hook form
+  const branchPossiblities = ["master", "with_router"] as const; //make sure to add "as const"
+
   const schema = z.object({
     name: z
       .string()
@@ -34,20 +36,21 @@ function Form() {
     age: z
       .number({ invalid_type_error: "Please fill a number" })
       .min(8, { message: "You are too young to send feedback" }),
-    github_user_id: z.string(),
-    reason_complaint: z.string(), //find how to also put "or null" as this field can be null
-    reason_appreciation: z.string(),
-    reason_other: z.string(),
+    github_user_id: z
+      .string()
+      .min(1, { message: "Please fill a valid github id" }),
+    reason_complaint: z.string().nullable(), //find how to also put "or null" as this field can be null
+    reason_appreciation: z.string().nullable(),
+    reason_other: z.string().nullable(),
     reason_other_description: z.string(),
-    branch: z.string(), //can be specified of value
+    branch: z.enum(branchPossiblities), //can be specified of value
   });
   type FormData = z.infer<typeof schema>;
   const { register, handleSubmit, getValues, formState } = useForm<FormData>({
     resolver: zodResolver(schema),
   });
 
-  const { errors, isValid } = formState;
-  //console.log(errors);
+  const { errors } = formState; //other props: isValid
 
   //handling values returned from react-hook-form and zod
   const handleFormSubmit = () => {
@@ -71,7 +74,16 @@ function Form() {
       ...otherFormValues,
     };
 
-    console.log(dataToSubmit);
+    console.log("Data submitted: ", dataToSubmit);
+
+    const alertMessage = `
+    Data was not saved anywhere...
+    See the data in the console
+
+    Help improve this app and add functionality
+    `;
+
+    alert(alertMessage);
   };
 
   const customOverlay = (
@@ -107,68 +119,89 @@ function Form() {
               <FormHelperText marginBottom={4}>
                 Your data won't get saved anywhere
               </FormHelperText>
-              <FormLabel>Name</FormLabel>
-              <Input {...register("name")} marginBottom={4} />
-              {errors.name && (
-                <FormErrorMessage>{errors.name.message}</FormErrorMessage>
-              )}
 
-              <FormLabel>Age</FormLabel>
-              <Input
-                {...register("age", { valueAsNumber: true })}
-                marginBottom={4}
-              />
-              {errors.age && (
-                <FormErrorMessage>{errors.age.message}</FormErrorMessage>
-              )}
+              <FormControl isInvalid={Boolean(errors.name)}>
+                <FormLabel>Name</FormLabel>
+                <Input {...register("name")} marginBottom={4} />
+                {errors.name && (
+                  <div>
+                    <FormErrorMessage>{errors.name.message}</FormErrorMessage>
+                    <p>demo</p>
+                  </div>
+                )}
+              </FormControl>
 
-              <FormControl isRequired>
+              <FormControl isInvalid={Boolean(errors.age)}>
+                <FormLabel>Age</FormLabel>
+                <Input
+                  {...register("age", { valueAsNumber: true })}
+                  marginBottom={4}
+                />
+                {errors.age && (
+                  <FormErrorMessage>{errors.age.message}</FormErrorMessage>
+                )}
+              </FormControl>
+
+              <FormControl
+                isRequired
+                isInvalid={Boolean(errors.github_user_id)}
+              >
                 <FormLabel>Github User ID</FormLabel>
                 <Input {...register("github_user_id")} marginBottom={4} />
+                {errors.github_user_id && (
+                  <FormErrorMessage>
+                    {errors.github_user_id.message}
+                  </FormErrorMessage>
+                )}
               </FormControl>
-              {errors.github_user_id && (
-                <FormErrorMessage>
-                  {errors.github_user_id.message}
-                </FormErrorMessage>
-              )}
 
-              <FormLabel>Reason for feedback</FormLabel>
-              <RadioGroup defaultValue="reason_for_feedback" marginBottom={2}>
-                <HStack spacing="30px">
-                  <Radio
-                    value="reason_Complaint" //just mere clicking sets the value to "reason_Complaint" ie "true" even if you disable it then. Solve this
-                    {...register("reason_complaint")}
-                    colorScheme="green"
-                    size="md"
-                  >
-                    Complaint
-                  </Radio>
-                  <Radio
-                    value="reason-Appreciation"
-                    {...register("reason_appreciation")}
-                    colorScheme="green"
-                    size="md"
-                  >
-                    Appreciation
-                  </Radio>
-                  <Radio
-                    value="reason-Other"
-                    {...register("reason_other")}
-                    colorScheme="green"
-                    size="md"
-                  >
-                    Other
-                  </Radio>
-                </HStack>
-              </RadioGroup>
-              {(errors.reason_appreciation ||
-                errors.reason_complaint ||
-                errors.reason_other) && (
-                <FormErrorMessage>
-                  Please click on each radio button to make form valid (fix this
-                  issue)
-                </FormErrorMessage>
-              )}
+              <FormControl
+                isInvalid={
+                  !(
+                    Boolean(errors.reason_appreciation) ||
+                    Boolean(errors.reason_complaint) ||
+                    Boolean(errors.reason_other)
+                  )
+                }
+              >
+                <FormLabel>Reason for feedback</FormLabel>
+                <RadioGroup defaultValue="reason_for_feedback" marginBottom={2}>
+                  <HStack spacing="30px">
+                    <Radio
+                      value="reason_Complaint" //just mere clicking sets the value to "reason_Complaint" and doesn't go back to null when another is selecte. Solve this
+                      {...register("reason_complaint")}
+                      colorScheme="green"
+                      size="md"
+                    >
+                      Complaint
+                    </Radio>
+                    <Radio
+                      value="reason-Complaint"
+                      {...register("reason_appreciation")}
+                      colorScheme="green"
+                      size="md"
+                    >
+                      Appreciation
+                    </Radio>
+                    <Radio
+                      value="reason-Other"
+                      {...register("reason_other")}
+                      colorScheme="green"
+                      size="md"
+                    >
+                      Other
+                    </Radio>
+                  </HStack>
+                </RadioGroup>
+                {(errors.reason_appreciation ||
+                  errors.reason_complaint ||
+                  errors.reason_other) && (
+                  <FormErrorMessage>
+                    Please click on each radio button to make form valid (fix
+                    this issue)
+                  </FormErrorMessage>
+                )}
+              </FormControl>
 
               <Textarea
                 placeholder="If other reason, please specify"
@@ -176,17 +209,21 @@ function Form() {
                 {...register("reason_other_description")}
               />
 
-              <FormLabel>Github Branch</FormLabel>
-              <Select
-                placeholder="Select github branch"
-                {...register("branch")}
+              <FormControl
+                isInvalid={Boolean(errors.branch)} /* deal with this issue */
               >
-                <option>Master</option>
-                <option>withRouter</option>
-              </Select>
-              {errors.branch && (
-                <FormErrorMessage>{errors.branch.message}</FormErrorMessage>
-              )}
+                <FormLabel>Github Branch</FormLabel>
+                <Select
+                  placeholder="Select github branch"
+                  {...register("branch")}
+                >
+                  <option value={"master"}>Master</option>
+                  <option value={"with_router"}>withRouter</option>
+                </Select>
+                {errors.branch && (
+                  <FormErrorMessage>{errors.branch.message}</FormErrorMessage>
+                )}
+              </FormControl>
             </FormControl>
           </ModalBody>
 
@@ -197,7 +234,7 @@ function Form() {
               onClick={handleSubmit(handleFormSubmit)} //handleSubmit from react-hook-form
               //handleSubmit( (data) => handleFormSubmit(data) ) is same as handleSubmit(handleFormSubmit)
               ml={3}
-              isDisabled={!isValid}
+              //isDisabled={!isValid}
             >
               Submit
             </Button>
