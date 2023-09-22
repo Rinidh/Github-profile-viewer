8dd7c4a8f50821df09893565d2f631c9b43ac0b3
Rinidh <rinidhdhokia@gmail.com>
2023-09-22 13:00:32 +0300

    Added error messages (not working) and custom overlay behind modal

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   FormControl,
+  FormErrorMessage,
   FormHelperText,
   FormLabel,
   HStack,
@@ -46,7 +47,9 @@ function Form() {
   });
 
   const { errors, isValid } = formState;
+  //console.log(errors);
 
+  //handling values returned from react-hook-form and zod
   const handleFormSubmit = () => {
     //handle some submit actions here
     const data = getValues(); //or just use data at the args ie (data: FieldValues) => {}
@@ -71,6 +74,16 @@ function Form() {
     console.log(dataToSubmit);
   };
 
+  const customOverlay = (
+    <ModalOverlay
+      bg="none" // or "rgba(0,0,1d,0.5)" or "blackAlpha.600" to achieve a translucent bluish background
+      backdropFilter="auto" // or direclty "blur(10px) hue-rotate(90deg)" instead of using other style props below
+      backdropBlur="8px"
+      backdropContrast="60%"
+    />
+  );
+  //if you have multiple custom modal overlays to use in diff places, handle them by using the state hook to keep track of the current cutom modal overlay
+
   return (
     <>
       <Button colorScheme="green" onClick={onOpen}>
@@ -83,92 +96,113 @@ function Form() {
         motionPreset="slideInBottom"
         size="2xl"
       >
-        <ModalOverlay>
-          <ModalContent>
-            <ModalHeader fontSize="lg" fontWeight="bold">
-              Please fill in these fields
-            </ModalHeader>
+        {customOverlay /* use custom <ModalOverlay> like this */}
+        <ModalContent>
+          <ModalHeader fontSize="lg" fontWeight="bold">
+            Please fill in these fields
+          </ModalHeader>
 
-            <ModalBody>
-              <FormControl>
-                <FormHelperText marginBottom={4}>
-                  Your data won't get saved anywhere
-                </FormHelperText>
-                <FormLabel>Name</FormLabel>
-                <Input {...register("name")} marginBottom={4} />
+          <ModalBody>
+            <FormControl>
+              <FormHelperText marginBottom={4}>
+                Your data won't get saved anywhere
+              </FormHelperText>
+              <FormLabel>Name</FormLabel>
+              <Input {...register("name")} marginBottom={4} />
+              {errors.name && (
+                <FormErrorMessage>{errors.name.message}</FormErrorMessage>
+              )}
 
-                <FormLabel>Age</FormLabel>
-                <Input
-                  {...register("age", { valueAsNumber: true })}
-                  marginBottom={4}
-                />
+              <FormLabel>Age</FormLabel>
+              <Input
+                {...register("age", { valueAsNumber: true })}
+                marginBottom={4}
+              />
+              {errors.age && (
+                <FormErrorMessage>{errors.age.message}</FormErrorMessage>
+              )}
 
-                <FormControl isRequired>
-                  <FormLabel>Github User ID</FormLabel>
-                  <Input {...register("github_user_id")} marginBottom={4} />
-                </FormControl>
-
-                <FormLabel>Reason for feedback</FormLabel>
-                <RadioGroup defaultValue="reason_for_feedback" marginBottom={2}>
-                  <HStack spacing="30px">
-                    <Radio
-                      value="reason_Complaint" //just mere clicking sets the value to "reason_Complaint" ie "true" even if you disable it then. Solve this
-                      {...register("reason_complaint")}
-                      colorScheme="green"
-                      size="md"
-                    >
-                      Complaint
-                    </Radio>
-                    <Radio
-                      value="reason-Appreciation"
-                      {...register("reason_appreciation")}
-                      colorScheme="green"
-                      size="md"
-                    >
-                      Appreciation
-                    </Radio>
-                    <Radio
-                      value="reason-Other"
-                      {...register("reason_other")}
-                      colorScheme="green"
-                      size="md"
-                    >
-                      Other
-                    </Radio>
-                  </HStack>
-                </RadioGroup>
-
-                <Textarea
-                  placeholder="If other reason, please specify"
-                  marginBottom={4}
-                  {...register("reason_other_description")}
-                />
-
-                <FormLabel>Github Branch</FormLabel>
-                <Select
-                  placeholder="Select github branch"
-                  {...register("branch")}
-                >
-                  <option>Master</option>
-                  <option>withRouter</option>
-                </Select>
+              <FormControl isRequired>
+                <FormLabel>Github User ID</FormLabel>
+                <Input {...register("github_user_id")} marginBottom={4} />
               </FormControl>
-            </ModalBody>
+              {errors.github_user_id && (
+                <FormErrorMessage>
+                  {errors.github_user_id.message}
+                </FormErrorMessage>
+              )}
 
-            <ModalFooter>
-              <Button onClick={onClose}>Cancel</Button>
-              <Button
-                colorScheme="green"
-                onClick={handleSubmit(handleFormSubmit)} //handleSubmit from react-hook-form
-                //handleSubmit( (data) => handleFormSubmit(data) ) is same as handleSubmit(handleFormSubmit)
-                ml={3}
-                isDisabled={!isValid}
+              <FormLabel>Reason for feedback</FormLabel>
+              <RadioGroup defaultValue="reason_for_feedback" marginBottom={2}>
+                <HStack spacing="30px">
+                  <Radio
+                    value="reason_Complaint" //just mere clicking sets the value to "reason_Complaint" ie "true" even if you disable it then. Solve this
+                    {...register("reason_complaint")}
+                    colorScheme="green"
+                    size="md"
+                  >
+                    Complaint
+                  </Radio>
+                  <Radio
+                    value="reason-Appreciation"
+                    {...register("reason_appreciation")}
+                    colorScheme="green"
+                    size="md"
+                  >
+                    Appreciation
+                  </Radio>
+                  <Radio
+                    value="reason-Other"
+                    {...register("reason_other")}
+                    colorScheme="green"
+                    size="md"
+                  >
+                    Other
+                  </Radio>
+                </HStack>
+              </RadioGroup>
+              {(errors.reason_appreciation ||
+                errors.reason_complaint ||
+                errors.reason_other) && (
+                <FormErrorMessage>
+                  Please click on each radio button to make form valid (fix this
+                  issue)
+                </FormErrorMessage>
+              )}
+
+              <Textarea
+                placeholder="If other reason, please specify"
+                marginBottom={4}
+                {...register("reason_other_description")}
+              />
+
+              <FormLabel>Github Branch</FormLabel>
+              <Select
+                placeholder="Select github branch"
+                {...register("branch")}
               >
-                Submit
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </ModalOverlay>
+                <option>Master</option>
+                <option>withRouter</option>
+              </Select>
+              {errors.branch && (
+                <FormErrorMessage>{errors.branch.message}</FormErrorMessage>
+              )}
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button onClick={onClose}>Cancel</Button>
+            <Button
+              colorScheme="green"
+              onClick={handleSubmit(handleFormSubmit)} //handleSubmit from react-hook-form
+              //handleSubmit( (data) => handleFormSubmit(data) ) is same as handleSubmit(handleFormSubmit)
+              ml={3}
+              isDisabled={!isValid}
+            >
+              Submit
+            </Button>
+          </ModalFooter>
+        </ModalContent>
       </Modal>
     </>
   );
